24c0886c9e3e3ace341def93c4915293c32f14f8
AniketShrivasta <94221116+AniketShrivasta@users.noreply.github.com>
2023-09-19 14:32:49 +0530

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,15 @@
+body {
+    font-family: Arial, sans-serif;
+    margin: 20px;
+}
+
+#editor {
+    border: 1px solid #ccc;
+    padding: 10px;
+    min-height: 200px;
+}
+
+button {
+    display: block;
+    margin-top: 10px;
+}
